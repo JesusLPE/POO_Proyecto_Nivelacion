666ace7b6a9a70a46c742fdca2147c1527e8172c
Joan-intriago <joanintriago7@gmail.com>
2026-05-14 15:37:17 -0500

    Delete ProyectoAula directory

--- ProyectoAula/models/docente.py
+++
@@ -1,9 +0,0 @@
-from .persona import Persona
-
-class Docente(Persona):
-    def __init__(self, nombre, apellido, email, password, especialidad):
-        super().__init__(nombre, apellido, email, password)
-        self.especialidad = especialidad
-
-    def obtener_rol(self):
-        return "docente"
