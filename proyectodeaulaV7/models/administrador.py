6b310c5a60856a1b55c4fc859875de28cf9667e9
Joan Intriago <joanintriago7@gmail.com>
2026-07-07 19:10:52 -0500

    Delete proyectodeaulaV7 directory

--- proyectodeaulaV7/models/administrador.py
+++
@@ -1,5 +0,0 @@
-from .persona import Persona
-
-class Administrador(Persona):
-    def obtener_rol(self):
-        return "administrador"
--- proyectodeaulaV7/models/coordinador.py
+++
@@ -1,5 +0,0 @@
-from .persona import Persona
-
-class Coordinador(Persona):
-    def obtener_rol(self):
-        return "coordinador"
