16bf0a27ece07a885429e50aa82e66609b3200c3
Joan Intriago <joanintriago7@gmail.com>
2026-07-07 19:09:59 -0500

    Delete proyectodeaula directory

--- proyectodeaula/models/coordinador.py
+++
@@ -1,5 +0,0 @@
-# coordinador.py
-from .persona import Persona
-class Coordinador(Persona):
-    def obtener_rol(self):
-        return "coordinador"
--- proyectodeaula/models/horario.py
+++
@@ -1,7 +0,0 @@
-# horario.py
-class Horario:
-    def __init__(self, id: int, dia: str, hora_inicio: str, hora_fin: str):
-        self._id = id
-        self._dia = dia
-        self._hora_inicio = hora_inicio
-        self._hora_fin = hora_fin
